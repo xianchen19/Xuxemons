ca543f8589bde72220ce2dc6d7b0c9b8e769f07d
andy25003 <andy16327@alumnes.ilernalleida.com>
2024-04-10 20:13:45 +0200

    a

--- xuxemons_backend/routes/api.php
+++ xuxemons_backend/routes/api.php
@@ -32,6 +32,7 @@ Route::middleware(['cors'])->group(function () {
     // Login
     Route::post('/userLogin', [LoginController::class, 'login']);
 
+<<<<<<< Updated upstream
     // Gestionar xuxemons
     // Rutas para xuxemons
    //Route::middleware(['role'])->group(function () {
@@ -55,4 +56,26 @@ Route::middleware(['cors'])->group(function () {
         Route::put('/configurations/{id}', [EvoConfigController::class, 'update']);
         Route::get('/inventario/{id}', [InventarioController::class, 'index']);
   // });
+=======
+//-----------Gestion usuario----------------
+    Route::get('/xuxemons/user', [xuxemonController::class, 'index']); //mostrar los xuxemons de ese usuario
+    Route::get('/xuxemons/verXuxemon', [xuxemonController::class, 'show']); //mostrar ese xuxemon especifico
+    Route::get('/random_xuxemon', [xuxemonController::class, 'randomXuxemon']); //random para usuario
+    Route::get('/give_chuche/{xuxemonId}/{candyAmount}', [xuxemonController::class, 'giveCandy']); //dar xuxe a xuxemon
+
+//-----------Gestion Administrador----------------
+   Route::middleware(['role'])->group(function () {
+        Route::post('/xuxemons/crearXuxemon', [xuxemonController::class, 'store']);//crear xuxemon
+        Route::put('/xuxemons/update', [xuxemonController::class, 'update']); //actualizar xuxemon
+        Route::delete('/xuxemons/delete', [xuxemonController::class, 'destroy']); //Admin
+        Route::get('/xuxemons', [xuxemonController::class, 'xuxemonAll']); //ver todos los xuxemons
+
+     
+        Route::get('/randomXuxemonAdmin', [xuxemonController::class, 'randomXuxemonAdmin']); //crear random Administrador
+        Route::get('/random_chucheAdmin', [inventarioController::class, 'randomChucheAdmin']);
+        Route::get('/configurations', [EvoConfigController::class, 'index']); //ver la configuracion 
+        Route::put('/configurations/{id}', [EvoConfigController::class, 'update']); //cambiar configuracion
+        Route::get('/inventario', [InventarioController::class, 'index']);
+  });
+>>>>>>> Stashed changes
 });
